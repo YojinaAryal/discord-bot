bf41cccd77b5e169817a641e9e4e923adce188c0
yojina <yojinaaryal2064@gmail.com>
2025-11-11 15:01:30 +0545

    Clean history without leaked token

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const {Client,GatewayIntentBits}= require("discord.js");
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 
-const client=new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
 });
 
-client.on("messageCreate",message => {
-    console.log(message.content);
-})
+client.on("messageCreate", (message) => {
+  console.log(message.content);
+});
 
-client.login(
-    "MTQzNzQ3MzY4OTczMjkxMTE2NA.GX8BIS.IZVQI1W_-FKXz5Y8uj2rC8WKzUDsBaObErQR2E"
-); 
+client.login(process.env.BOT_TOKEN);
